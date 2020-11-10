7d772473a1c7c78c0cf24d47d8e3d0b34bdaed83
lamas250 <igorlamas1994@gmail.com.br>
2020-11-10 11:03:43 -0300

    scss steup

---
+++ src/pages/Home/HomeComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>Dash</p>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Login/LoginComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="login">
+
+        <div class="info">
+            <h1>Login do Sistema</h1>
+            <p>E-mail</p>
+        </div>
+
+        <div class="footer">
+            <p>Rodape</p>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" src="./style.scss" scoped/>
